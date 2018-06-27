49576a83fc75ef8d5771a2566875dcf25d221572
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2018-06-27 17:19:29 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+import os
+
+name = 'sqlalchemy-stubs'
+description = 'Experimental SQLAlchemy stubs'
+
+def find_stub_files():
+    result = []
+    for root, dirs, files in os.walk(name):
+        for file in files:
+            if file.endswith('.pyi'):
+                if os.path.sep in root:
+                    root = root.split(os.path.sep, 1)[-1]
+                    file = os.path.join(root, file)
+                result.append(file)
+    return result
+
+setup(name='sqlalchemy-stubs',
+      version='0.1',
+      description=description,
+      long_description=description,
+      author='Ivan Levkivskyi',
+      author_email='levkivskyi@gmail.com',
+      license='MIT License',
+      py_modules=[],
+      packages=['sqlalchemy-stubs'],
+      package_data={'sqlalchemy-stubs': find_stub_files()}
+)
