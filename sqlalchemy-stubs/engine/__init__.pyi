2bb9dc5cb2e8e9b159ae6210fcc59776a5b9f12c
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2018-06-29 00:23:18 +0100

    Add basic engine stubs

--- sqlalchemy-stubs/engine/__init__.pyi
+++ sqlalchemy-stubs/engine/__init__.pyi
@@ -1,3 +1,31 @@
-from typing import Any
+from .interfaces import (
+    Connectable as Connectable,
+    CreateEnginePlugin as CreateEnginePlugin,
+    Dialect as Dialect,
+    ExecutionContext as ExecutionContext,
+    ExceptionContext as ExceptionContext,
+    Compiled as Compiled,
+    TypeCompiler as TypeCompiler
+)
 
-def __getattr__(attr: str) -> Any: ...
+from .base import (
+    Connection as Connection,
+    Engine as Engine,
+    NestedTransaction as NestedTransaction,
+    RootTransaction as RootTransaction,
+    Transaction as Transaction,
+    TwoPhaseTransaction as TwoPhaseTransaction,
+)
+
+from .result import (
+    BaseRowProxy as BaseRowProxy,
+    BufferedColumnResultProxy as BufferedColumnResultProxy,
+    BufferedColumnRow as BufferedColumnRow,
+    BufferedRowResultProxy as BufferedRowResultProxy,
+    FullyBufferedResultProxy as FullyBufferedResultProxy,
+    ResultProxy as ResultProxy,
+    RowProxy as RowProxy,
+)
+
+def create_engine(*args, **kwargs): ...
+def engine_from_config(configuration, prefix: str = ..., **kwargs): ...
