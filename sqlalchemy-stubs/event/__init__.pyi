e5b8e0b698860591939d6aabcac8414fc3f74421
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2018-07-02 21:48:01 +0100

    Add event modules

--- sqlalchemy-stubs/event/__init__.pyi
+++ sqlalchemy-stubs/event/__init__.pyi
@@ -1,3 +1,10 @@
-from typing import Any
-
-def __getattr__(attr: str) -> Any: ...
+from .api import (
+    CANCEL as CANCEL,
+    NO_RETVAL as NO_RETVAL,
+    listen as listen,
+    listens_for as listens_for,
+    remove as remove,
+    contains as contains
+)
+from .base import Events as Events, dispatcher as dispatcher
+from .attr import RefCollection as RefCollection
