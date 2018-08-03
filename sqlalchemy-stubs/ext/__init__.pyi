1b11765576c83f72104dcc8d6cca02aa881a9920
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2018-08-03 14:10:48 +0100

    Add ORM extensions

--- sqlalchemy-stubs/ext/__init__.pyi
+++ sqlalchemy-stubs/ext/__init__.pyi
@@ -1,3 +1,14 @@
-from typing import Any
-
-def __getattr__(attr: str) -> Any: ...
+from . import (
+    declarative as declarative,
+    associationproxy as associationproxy,
+    automap as automap,
+    baked as baked,
+    compiler as compiler,
+    horizontal_shard as horizontal_shard,
+    hybrid as hybrid,
+    indexable as indexable,
+    instrumentation as instrumentation,
+    mutable as mutable,
+    orderinglist as orderinglist,
+    serializer as serializer
+)
--- sqlalchemy-stubs/ext/declarative/__init__.pyi
+++ sqlalchemy-stubs/ext/declarative/__init__.pyi
@@ -1,3 +1,13 @@
-from typing import Any
-
-def __getattr__(attr: str) -> Any: ...
+from .api import (
+    declarative_base as declarative_base,
+    synonym_for as synonym_for,
+    comparable_using as comparable_using,
+    instrument_declarative as instrument_declarative,
+    ConcreteBase as ConcreteBase,
+    AbstractConcreteBase as AbstractConcreteBase,
+    DeclarativeMeta as DeclarativeMeta,
+    DeferredReflection as DeferredReflection,
+    has_inherited_table as has_inherited_table,
+    declared_attr as declared_attr,
+    as_declarative as as_declarative
+)
