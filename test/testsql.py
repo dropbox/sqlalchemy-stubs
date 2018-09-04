581662fc30014f6fa1534edb81779f674d251a89
Ivan Levkivskyi <levkivskyi@gmail.com>
2018-09-04 17:29:14 +0100

    Add some testing infrastructure (#13)
    This includes only basic infra

---
+++ test/testsql.py
@@ -0,0 +1,58 @@
+"""Mypy style test cases for SQLAlchemy stubs and plugin."""
+
+import os
+import os.path
+import sys
+
+import pytest  # type: ignore  # no pytest in typeshed
+
+from mypy.test.config import test_temp_dir
+from mypy.test.data import DataDrivenTestCase, DataSuite
+from mypy.test.helpers import assert_string_arrays_equal
+from mypy import api
+
+this_file_dir = os.path.dirname(os.path.realpath(__file__))
+prefix = os.path.dirname(this_file_dir)
+
+# Locations of test data files such as test case descriptions (.test).
+test_data_prefix = os.path.join(prefix, 'test', 'test-data')
+
+
+class SQLDataSuite(DataSuite):
+    files = ['sqlalchemy-basics.test']
+    data_prefix = test_data_prefix
+
+    def run_case(self, testcase: DataDrivenTestCase) -> None:
+
+        assert testcase.old_cwd is not None, "test was not properly set up"
+        mypy_cmdline = [
+            '--show-traceback',
+            '--no-silence-site-packages',
+        ]
+        py2 = testcase.name.lower().endswith('python2')
+        if py2:
+            mypy_cmdline.append('--py2')
+        else:
+            mypy_cmdline.append('--python-version={}'.format('.'.join(map(str,
+                                                                          sys.version_info[:2]))))
+
+        # Write the program to a file.
+        program_path = os.path.join(test_temp_dir, 'main.py')
+        mypy_cmdline.append(program_path)
+        with open(program_path, 'w') as file:
+            for s in testcase.input:
+                file.write('{}\n'.format(s))
+        output = []
+        # Type check the program.
+        out, err, returncode = api.run(mypy_cmdline)
+        # split lines, remove newlines, and remove directory of test case
+        for line in (out + err).splitlines():
+            if line.startswith(test_temp_dir + os.sep):
+                output.append(line[len(test_temp_dir + os.sep):].rstrip("\r\n"))
+            else:
+                output.append(line.rstrip("\r\n"))
+        # Remove temp file.
+        os.remove(program_path)
+        assert_string_arrays_equal(testcase.output, output,
+                                   'Invalid output ({}, line {})'.format(
+                                   testcase.file, testcase.line))
