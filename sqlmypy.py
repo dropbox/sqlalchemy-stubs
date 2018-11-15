9664932b2aac84941725020a238f5facccbb62ae
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2018-11-14 23:04:33 -0800

    A sketchy implementation of basic __init__ plugin

--- sqlmypy.py
+++ sqlmypy.py
@@ -1,14 +1,21 @@
 from mypy.plugin import Plugin
-from mypy.nodes import NameExpr, Expression, StrExpr, TypeInfo, ClassDef, Block, SymbolTable, SymbolTableNode, GDEF
-from mypy.types import UnionType, NoneTyp, Instance, Type, CallableType, AnyType, TypeOfAny
+from mypy.nodes import(
+    NameExpr, Expression, StrExpr, TypeInfo, ClassDef, Block, SymbolTable, SymbolTableNode, GDEF,
+    AssignmentStmt, CallExpr, RefExpr, Argument, Var, ARG_OPT
+)
+from mypy.types import (
+    UnionType, NoneTyp, Instance, Type, CallableType, AnyType, TypeOfAny, Overloaded
+)
 from mypy.erasetype import erase_typevars
+from mypy.maptype import map_instance_to_supertype
 
-from typing import Optional, TYPE_CHECKING, Callable
+from typing import Optional, TYPE_CHECKING, Callable, Set, List
 
 if TYPE_CHECKING:
-    from mypy.plugin import FunctionContext
+    from mypy.plugin import FunctionContext, ClassDefContext
 
-DECL_BASES = set()
+DECL_BASES = set()  # type: Set[str]
+DECL_BASES.add('base.Base')
 
 
 class BasicSQLAlchemyPlugin(Plugin):
@@ -23,6 +30,96 @@ class BasicSQLAlchemyPlugin(Plugin):
         if fullname == 'sqlalchemy.ext.declarative.api.declarative_base':
             return decl_info_hook
         return None
+
+    def get_class_decorator_hook(self, fullname: str) -> Optional[Callable[['ClassDefContext'], None]]:
+        if fullname == 'sqlalchemy.ext.declarative.api.as_declarative':
+            return decl_deco_hook
+        return None
+
+    def get_base_class_hook(self, fullname: str) -> Optional[Callable[['ClassDefContext'], None]]:
+        if fullname in DECL_BASES:
+            return add_init_hook
+        return None
+
+
+def _get_column_argument(call: CallExpr, name: str) -> Optional[Expression]:
+    """Return the expression for the specific argument."""
+    # This is super sketchy.
+    callee_node = call.callee.node
+    callee_node_type = callee_node.names['__init__'].type
+    assert isinstance(callee_node_type, Overloaded)
+    if isinstance(call.args[0], StrExpr):
+        overload_index = 0
+    else:
+        overload_index = 1
+    callee_type = callee_node_type.items()[overload_index]
+    if not callee_type:
+        return None
+
+    argument = callee_type.argument_by_name(name)
+    if not argument:
+        return None
+    assert argument.name
+
+    for i, (attr_name, attr_value) in enumerate(zip(call.arg_names, call.args)):
+        if argument.pos is not None and not attr_name and i == argument.pos - 1:
+            return attr_value
+        if attr_name == argument.name:
+            return attr_value
+    return None
+
+
+def add_init_hook(ctx: 'ClassDefContext') -> None:
+    from mypy.plugins.common import _add_method
+    if '__init__' in ctx.cls.info.names:
+        # Don't override existing definition.
+        return
+    col_types = []  # type: List[Type]
+    col_names = []  # type: List[str]
+    engine_info = ctx.api.named_type_or_none('sqlalchemy.sql.type_api.TypeEngine').type
+    for stmt in ctx.cls.defs.body:
+        if (isinstance(stmt, AssignmentStmt) and isinstance(stmt.lvalues[0], NameExpr) and
+                isinstance(stmt.rvalue, CallExpr) and
+                isinstance(stmt.rvalue.callee, RefExpr) and
+                stmt.rvalue.callee.fullname == 'sqlalchemy.sql.schema.Column'):
+            # OK, this is what we a looking for.
+            col_names.append(stmt.lvalues[0].name)
+            # First try the easy way...
+            if isinstance(stmt.type, Instance):
+                col_types.append(stmt.type.args[0])
+                continue
+            # ...otherwise, the hard way (hard because types are not inferred yet,
+            # we are in semantic analysis pass)
+            typ_arg = _get_column_argument(stmt.rvalue, 'type_')
+            if isinstance(typ_arg, RefExpr):
+                typ_name = typ_arg.fullname
+            elif isinstance(typ_arg, CallExpr) and isinstance(typ_arg.callee, RefExpr):
+                typ_name = typ_arg.callee.fullname
+            else:
+                col_types.append(AnyType(TypeOfAny.special_form))
+                continue
+            typ = ctx.api.named_type_or_none(typ_name)
+            if typ and typ.type.has_base('sqlalchemy.sql.type_api.TypeEngine'):
+                # Using maptype at this stage is dangerous, since if there is an import cycle,
+                # the result is unpredictable.
+                engine = map_instance_to_supertype(typ, engine_info)
+                if engine.args and isinstance(engine.args[0], Instance):
+                    # OK, the column type already analyzed, we are good to go
+                    col_types.append(engine.args[0])
+                    continue
+            # Can't figure out type, fall back to Any
+            col_types.append(AnyType(TypeOfAny.special_form))
+    init_args = []  # type: List[Argument]
+    for typ, name in zip(col_types, col_names):
+        typ = UnionType([typ, NoneTyp()])
+        var = Var(name, typ)
+        i_arg = Argument(variable=var, type_annotation=typ, initializer=None, kind=ARG_OPT)
+        init_args.append(i_arg)
+    _add_method(ctx, '__init__', init_args, NoneTyp())
+
+
+def decl_deco_hook(ctx: 'ClassDefContext') -> None:
+    DECL_BASES.add(ctx.cls.fullname)
 
 
 def decl_info_hook(ctx):
