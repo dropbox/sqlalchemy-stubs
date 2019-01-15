c056851f8ea799cfb7290afa5b6850e03860c6ff
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2019-01-15 18:00:37 +0000

    Update model __init__ hooks

--- sqlmypy.py
+++ sqlmypy.py
@@ -2,22 +2,26 @@ from mypy.plugin import Plugin, FunctionContext, ClassDefContext
 from mypy.plugins.common import add_method
 from mypy.nodes import(
     NameExpr, Expression, StrExpr, TypeInfo, ClassDef, Block, SymbolTable, SymbolTableNode, GDEF,
-    AssignmentStmt, CallExpr, RefExpr, Argument, Var, ARG_OPT
+    CallExpr, Argument, Var, ARG_STAR2
 )
 from mypy.types import (
     UnionType, NoneTyp, Instance, Type, CallableType, AnyType, TypeOfAny, Overloaded
 )
 from mypy.erasetype import erase_typevars
-from mypy.maptype import map_instance_to_supertype
 
-from typing import Optional, Callable, List
+from typing import Optional, Callable, Dict, TYPE_CHECKING
+if TYPE_CHECKING:
+    from typing_extensions import Final
+
+COLUMN_NAME = 'sqlalchemy.sql.schema.Column'  # type: Final
+RELATIONSHIP_NAME = 'sqlalchemy.orm.relationships.RelationshipProperty'  # type: Final
 
 
 def is_declarative(info: TypeInfo) -> bool:
     """Check if this is a subclass of a declarative base."""
     if info.mro:
         for base in info.mro:
-            metadata = info.metadata.get('sqlalchemy')
+            metadata = base.metadata.get('sqlalchemy')
             if metadata and metadata.get('declarative_base'):
                 return True
     return False
@@ -38,9 +42,9 @@ class BasicSQLAlchemyPlugin(Plugin):
         using flags 'primary_key', 'nullable', 'uselist', etc.
     """
     def get_function_hook(self, fullname: str) -> Optional[Callable[[FunctionContext], Type]]:
-        if fullname == 'sqlalchemy.sql.schema.Column':
+        if fullname == COLUMN_NAME:
             return column_hook
-        if fullname == 'sqlalchemy.orm.relationships.RelationshipProperty':
+        if fullname == RELATIONSHIP_NAME:
             return relationship_hook
         sym = self.lookup_fully_qualified(fullname)
         if sym and isinstance(sym.node, TypeInfo):
@@ -95,51 +99,19 @@ def _get_column_argument(call: CallExpr, name: str) -> Optional[Expression]:
 
 
 def add_init_hook(ctx: ClassDefContext) -> None:
+    """Add a dummy __init__() to a model and record it is generated.
+
+    Instantiation will be checked more precisely when we inferred types
+    (using get_function_hook and model_hook).
+    """
     if '__init__' in ctx.cls.info.names:
         # Don't override existing definition.
         return
-    col_types = []  # type: List[Type]
-    col_names = []  # type: List[str]
-    engine_info = ctx.api.named_type_or_none('sqlalchemy.sql.type_api.TypeEngine').type
-    for stmt in ctx.cls.defs.body:
-        if (isinstance(stmt, AssignmentStmt) and isinstance(stmt.lvalues[0], NameExpr) and
-                isinstance(stmt.rvalue, CallExpr) and
-                isinstance(stmt.rvalue.callee, RefExpr) and
-                stmt.rvalue.callee.fullname == 'sqlalchemy.sql.schema.Column'):
-            # OK, this is what we a looking for.
-            col_names.append(stmt.lvalues[0].name)
-            # First try the easy way...
-            if isinstance(stmt.type, Instance):
-                col_types.append(stmt.type.args[0])
-                continue
-            # ...otherwise, the hard way (hard because types are not inferred yet,
-            # we are in semantic analysis pass)
-            typ_arg = _get_column_argument(stmt.rvalue, 'type_')
-            if isinstance(typ_arg, RefExpr):
-                typ_name = typ_arg.fullname
-            elif isinstance(typ_arg, CallExpr) and isinstance(typ_arg.callee, RefExpr):
-                typ_name = typ_arg.callee.fullname
-            else:
-                col_types.append(AnyType(TypeOfAny.special_form))
-                continue
-            typ = ctx.api.named_type_or_none(typ_name)
-            if typ and typ.type.has_base('sqlalchemy.sql.type_api.TypeEngine'):
-                # Using maptype at this stage is dangerous, since if there is an import cycle,
-                # the result is unpredictable.
-                engine = map_instance_to_supertype(typ, engine_info)
-                if engine.args and isinstance(engine.args[0], Instance):
-                    # OK, the column type already analyzed, we are good to go
-                    col_types.append(engine.args[0])
-                    continue
-            # Can't figure out type, fall back to Any
-            col_types.append(AnyType(TypeOfAny.special_form))
-    init_args = []  # type: List[Argument]
-    for typ, name in zip(col_types, col_names):
-        typ = UnionType([typ, NoneTyp()])
-        var = Var(name, typ)
-        i_arg = Argument(variable=var, type_annotation=typ, initializer=None, kind=ARG_OPT)
-        init_args.append(i_arg)
-    add_method(ctx, '__init__', init_args, NoneTyp())
+    typ = AnyType(TypeOfAny.special_form)
+    var = Var('kwargs', typ)
+    kw_arg = Argument(variable=var, type_annotation=typ, initializer=None, kind=ARG_STAR2)
+    add_method(ctx, '__init__', [kw_arg], NoneTyp())
+    ctx.cls.info.metadata.setdefault('sqlalchemy', {})['generated_init'] = True
 
 
 def decl_deco_hook(ctx: ClassDefContext) -> None:
@@ -176,6 +148,40 @@ def decl_info_hook(ctx):
 
 
 def model_hook(ctx: FunctionContext) -> Type:
+    """More precise model instantiation check.
+
+    Note: sub-models are not supported.
+    Note: this is still not perfect, since the context for inference of
+          argument types is 'Any'.
+    """
+    assert isinstance(ctx.default_return_type, Instance)
+    model = ctx.default_return_type.type
+    metadata = model.metadata.get('sqlalchemy')
+    if not metadata or not metadata.get('generated_init'):
+        return ctx.default_return_type
+
+    # Collect column names and types defined in the model
+    # TODO: cache this?
+    expected_types = {}  # type: Dict[str, Type]
+    for name, sym in model.names.items():
+        if isinstance(sym.node, Var) and isinstance(sym.node.type, Instance):
+            tp = sym.node.type
+            if tp.type.fullname() in (COLUMN_NAME, RELATIONSHIP_NAME):
+                assert len(tp.args) == 1
+                expected_types[name] = tp.args[0]
+
+    assert len(ctx.arg_names) == 1  # only **kwargs in generated __init__
+    assert len(ctx.arg_types) == 1
+    for actual_name, actual_type in zip(ctx.arg_names[0], ctx.arg_types[0]):
+        if actual_name not in expected_types:
+            ctx.api.fail('Unexpected column "{}" for model "{}"'.format(actual_name, model.name()),
+                         ctx.context)
+            continue
+        # Using private API to simplify life.
+        ctx.api.check_subtype(actual_type, expected_types[actual_name],
+                             ctx.context,
+                             'Incompatible type for "{}" of "{}"'.format(actual_name, model.name()),
+                             'got', 'expected')
     return ctx.default_return_type
 
 
@@ -222,7 +228,7 @@ def column_hook(ctx: FunctionContext) -> Type:
             nullable = not parse_bool(primary_args[0])
         else:
             nullable = True
-    # TODO: Add support for literal types when they will be available.
+    # TODO: Add support for literal types.
 
     if not nullable:
         return ctx.default_return_type
