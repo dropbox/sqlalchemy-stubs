9a4a6dba4f4e1d8ec381dc2fc070f355b52762d3
Rafal Stozek <rafal.stozek@gmail.com>
2019-06-30 11:01:06 +0200

    fix: typed queries return tuple for single-value non-entity queries

--- sqlmypy.py
+++ sqlmypy.py
@@ -401,18 +401,23 @@ def session_query_hook(ctx: MethodContext) -> Type:
         session.query(User) -> Query[User]
         session.query(User, Order) -> Query[Tuple[User, Order]]
         session.query(User, User.id) -> Query[Tuple[User, int]]
+        session.query(User.id) -> Query[Tuple[int]]
     """
     # TODO take a look at Session.query_cls? Do we have to do this with generics?
 
+    cnt_entities = 0
+
     def map_arg(arg: Type) -> Type:
+        nonlocal cnt_entities
         # A model class (well, in this case a callable constructor)
         # Example:
         # session.query(Employee) -> Query[Employee]
         if isinstance(arg, CallableType) and arg.is_type_obj():
+            cnt_entities += 1
             return arg.ret_type
 
         # Example:
-        # session.query(Employee.id) -> Query[int]
+        # session.query(Employee.id, ...) -> Query[int, ...]
         if isinstance(arg, Instance) and arg.type.fullname() == COLUMN_NAME:
             assert len(arg.args) == 1, "Column[...] should have only one generic argument"
             return arg.args[0]
@@ -422,7 +427,9 @@ def session_query_hook(ctx: MethodContext) -> Type:
     # take positional arguments and map them
     args = [map_arg(arg) for arg in ctx.arg_types[0]]
 
-    if len(args) == 1:
+    if len(args) == 1 and (cnt_entities == 1 or isinstance(args[0], AnyType)):
+        # when there is a single class passed as an argument
+        # or when we can't detect what the single argument is (Any)
         final_arg = args[0]
     else:
         fallback = ctx.api.named_type('sqlalchemy.util._collections.AbstractKeyedTuple')
